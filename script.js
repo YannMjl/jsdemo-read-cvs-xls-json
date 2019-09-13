cd9e946e711a4b40768c364ab71f902ee61aac50
Yann Mulonda <yannmjl@gmail.com>
2019-09-13 14:05:17 -0500

    display excel data

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ $(document).ready(function () {
 
     $("#headerTitle").hide(300).show(1500);
 
-    // read Excel file and convert to json format
+    // read Excel file and convert to json format using fetch
     fetch('./soccer_players.xlsx').then(function (res) {
         /* get the data as a Blob */
         if (!res.ok) throw new Error("fetch failed");
         return res.arrayBuffer();
-    }).then(function (ab) {
+    })
+    .then(function (ab) {
         /* parse the data when it is received */
         var data = new Uint8Array(ab);
         var workbook = XLSX.read(data, {
             type: "array"
         });
 
-        /* DO SOMETHING WITH workbook HERE */
         /* *****************************************************************
-        *    Converting Excel value to Json                                *
+        * DO SOMETHING WITH workbook: Converting Excel value to Json       *
         ********************************************************************/
         var first_sheet_name = workbook.SheetNames[0];
         /* Get worksheet */
@@ -34,9 +34,38 @@ $(document).ready(function () {
         /************************ End of conversion ************************/
 
         console.log(_JsonData);
+
+        $.each(_JsonData, function (index, value) {
+
+            $('#showExcel').append(
+
+                '<tr>' +
+                    '<th scope="row">' +
+                        value['FIRST NAME'] +
+                    '</th>' + 
+                    '<td>' +
+                        value['LAST NAME'] +
+                    '</td>' +  
+                    '<td>' +
+                        '<span class="badge badge-primary badge-pill p-2">' +
+                            value.AGE +
+                        '</span>' +
+                    '</td>' +
+                    '<td>' +
+                        value.CLUB +
+                    '</td>' +
+                    '<td>' +
+                        value.CITY +
+                    '</td>' +
+                '</tr>'
+
+            );
+
+        });
+
     });
 
-    // read csv file and convert to json format
+    // read csv file and convert to json format using ajax
     $.ajax({
 
         type: 'GET',
@@ -62,11 +91,11 @@ $(document).ready(function () {
 
                     '<li class="list-group-item d-flex justify-content-between align-items-center">' + 
                         
-                        '<span style="margin-right: 2rem; font-size: 2rem; font-weight: bold; color: #37474F">' +
+                        '<span style="width: 15%; margin-right: 1.5rem; font-size: 1rem; font-weight: bold; color: #37474F">' +
                             value['FIRST NAME'] +
                         '</span>' +
 
-                        '<span style="margin-right: 2rem; font-size: 2rem; font-weight: bold; color: #37474F">' +
+                        '<span style="width: 15%; margin-right: 2rem; font-size: 1rem;  color: #37474F">' +
                             value['LAST NAME'] +
                         '</span>' +
 
@@ -74,7 +103,7 @@ $(document).ready(function () {
                             value.CITY +
                         '</span>' +
 
-                        '<span class="badge warning-color-dark badge-pill">' + 
+                        '<span class="badge warning-color-dark badge-pill">' +
                             value['PHONE NUMBER'] +
                         '</span>' +
 
