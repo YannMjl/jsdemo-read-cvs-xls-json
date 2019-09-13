99e596728b2212ff3c9c30b83d90688ca23ae992
Yann Mulonda <yannmjl@gmail.com>
2019-09-13 11:28:35 -0500

    display first name on the table

--- script.js
+++ script.js
@@ -80,6 +80,10 @@ $(document).ready(function () {
                     '<li class="list-group-item d-flex justify-content-between align-items-center">' + 
                         
                         '<span style="margin-right: 2rem; font-size: 2rem; font-weight: bold; color: #37474F">' +
+                            value['FIRST NAME'] +
+                        '</span>' +
+
+                        '<span style="margin-right: 2rem; font-size: 2rem; font-weight: bold; color: #37474F">' +
                             value['LAST NAME'] +
                         '</span>' +
 
